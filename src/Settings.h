a962b9a0701a7a796516b4d265b2bf6b0108e556
ForestJ316 <91183260+ForestJ316@users.noreply.github.com>
2024-04-22 19:14:39 +0100

    Version 1.0.0

---
+++ src/Settings.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class Settings
+{
+public:
+	static void ReadIniSettings();
+
+	static void ReadBoolSetting(CSimpleIniA& a_ini, const char* a_sectionName, const char* a_settingName, bool& a_setting);
+
+	// General
+	static inline bool bDisableLevelUpHealth = false;
+	static inline bool bDisableLevelUpAP = false;
+};
